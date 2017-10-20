1ee22fb94b884d300d8c5241e286c0dc4f5ef339
Nat Keane <ntlkeane.nk@gmail.com>
2017-10-20 12:21:09 -0500

    git commit

--- maze.js
+++ maze.js
@@ -1,8 +1,95 @@
 "use strict";
 window.onload=function(){
 
-var blinky = document.querySelector("#boundary1");
+var loser = false;
+
+var sessionStart = false;
+
+var blinky = document.querySelectorAll(".boundary");
+
+for(var count=0; count<blinky.length-1; count++){
+blinky[count].addEventListener("mouseover", gameOver);
+}
 blinky.addEventListener("mouseover", function(){
   blinky.classList.add("youlose");
 })
+}
+    document.getElementById("end").addEventListener("mouseover",goal);
+
+    document.getElementById("start").addEventListener("click",beginGame);
+
+}
+
+
+
+function gameOver(){
+
+    if(sessionStart){
+
+        setBoundaryCSS("boundary youlose");
+
+        loser = true;
+
+    }
+
+}
+
+
+
+function goal(){
+
+    if(sessionStart){
+
+        if(!loser){
+
+            setStat("You Win!");
+
+        }
+
+        else{
+
+            setStat("You Lose!");
+
+        }
+
+        sessionStart = false;
+
+    }
+
+}
+
+
+
+function beginGame(){
+
+    setBoundaryCSS("boundary");
+
+    sessionStart = true;
+
+    loser = false;
+
+    setStat("Game In Session");
+
+}
+
+
+
+function setBoundaryCSS(cssName){
+
+    var blinky = document.querySelectorAll(".boundary");
+
+    for (var i = 0; i < blinky.length-1; i++) {
+
+        blinky[i].className = cssName;
+
+    }
+
+}
+
+
+
+function setStat(stat){
+
+    document.getElementById("status").innerHTML = stat;
+
 }
